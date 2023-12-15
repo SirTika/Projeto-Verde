4819f8ecc01aaae48864d0f11a6de7a664904c38
SirTika <tikalevsta@gmail.com>
2023-12-15 17:11:13 -0300

    commit

--- js/content.js
+++ js/content.js
@@ -1,18 +1,23 @@
-items.forEach(function(item) {
-    item.addEventListener('click', function() {
-        var fileName;
-        if (item.dataset.name === 'Girassol Flores Iniciantes') {
-            fileName = 'txts/girassol.txt';
-        } else if (item.dataset.name === 'Alface Verduras Iniciantes') {
-            fileName = 'txts/alface.txt';
-        }
-        // e assim por diante para os outros itens
+document.addEventListener('DOMContentLoaded', function() {
+    var items = document.querySelectorAll('.item');
+    var descriptionBox = document.querySelector('.content');
 
-        fetch(fileName)
-            .then(response => response.text())
-            .then(data => {
-                descriptionBox.innerHTML = data;
-            })
-            .catch(error => console.error(error));
+    items.forEach(function(item) {
+        item.addEventListener('click', function() {
+            var fileName;
+            if (item.dataset.name === 'Girassol Flores Iniciantes') {
+                fileName = 'txts/girassol.txt';
+            } else if (item.dataset.name === 'Alface Verduras Iniciantes') {
+                fileName = 'txts/alface.txt';
+            }
+            // e assim por diante para os outros itens
+
+            fetch(fileName)
+                .then(response => response.text())
+                .then(data => {
+                    descriptionBox.innerHTML = data;
+                })
+                .catch(error => console.error(error));
+        });
     });
 });
