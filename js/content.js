11db17c5920196ed6a72ff959ef69ff174af5a88
SirTika <tikalevsta@gmail.com>
2023-12-15 16:33:07 -0300

    commit

--- js/content.js
+++ js/content.js
@@ -1,5 +1,18 @@
-fetch('content.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('content').innerHTML = data;
-  });
+items.forEach(function(item) {
+    item.addEventListener('click', function() {
+        var fileName;
+        if (item.dataset.name === 'Girassol Flores Iniciantes') {
+            fileName = 'txts/girassol.txt';
+        } else if (item.dataset.name === 'Alface Verduras Iniciantes') {
+            fileName = 'txts/alface.txt';
+        }
+        // e assim por diante para os outros itens
+
+        fetch(fileName)
+            .then(response => response.text())
+            .then(data => {
+                descriptionBox.innerHTML = data;
+            })
+            .catch(error => console.error(error));
+    });
+});
